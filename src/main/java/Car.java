26d293a75ac6ec5039239aa4aefbe1eea3796986
Whit3Flam3 <50198595+Whit3Flam3@users.noreply.github.com>
2019-10-15 18:07:06 -0500

    Project: Car Commit 1 Notes: Completed.

---
+++ src/main/java/Car.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dmosi
+ */
+public class Car {
+	private int speed = 0;
+	private String make = "Chevy";
+	private int modelYear = 2000;
+		
+	public Car (String n, int Model)
+	{	
+		make = n;
+		modelYear = Model; 
+	}
+	
+	public int getyearModel ()
+	{
+		return modelYear;
+	}
+	
+	public String getMake ()
+	{
+		return make;
+	}
+	
+	public int getSpeed ()
+	{
+		return speed;
+	}	
+		
+	public void accelerate ()
+	{
+		if (speed !=90)
+			speed = speed +5;
+	}
+	
+	public void brake ()
+	{
+		if (speed !=0)
+			speed = speed -5;
+	}
+}
